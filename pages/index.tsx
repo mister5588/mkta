fb0b320ed6186bac7e33f2f3fe75718530ae8a41
Mat5588 <65315400+mister5588@users.noreply.github.com>
2024-02-19 01:32:33 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,97 +1,18 @@
-import {
-  useContract,
-  useNFTs,
-  useOwnedNFTs,
-  useValidDirectListings,
-  useValidEnglishAuctions,
-} from "@thirdweb-dev/react";
-import { useRouter } from "next/router";
-import React, { useState } from "react";
-import Container from "../components/Container/Container";
-import ListingWrapper from "../components/ListingWrapper/ListingWrapper";
-import NFTGrid from "../components/NFT/NFTGrid";
-import Skeleton from "../components/Skeleton/Skeleton";
-import {
-  MARKETPLACE_ADDRESS,
-  NFT_COLLECTION_ADDRESS,
-} from "../const/contractAddresses";
-import styles from "../styles/Profile.module.css";
-import randomColor from "../util/randomColor";
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
-const [randomColor1, randomColor2, randomColor3, randomColor4] = [
-  randomColor(),
-  randomColor(),
-  randomColor(),
-  randomColor(),
-];
-
-export default function ProfilePage() {
+const IndexPage = () => {
   const router = useRouter();
-  const [tab, setTab] = useState<"nfts" | "listings" | "auctions">("listings"); // Set "listings" as the default active tab
 
-  const { contract: nftCollection } = useContract(NFT_COLLECTION_ADDRESS);
-  const { contract: marketplace } = useContract(
-    MARKETPLACE_ADDRESS,
-    "marketplace-v3"
-  );
-  const { data: ownedNfts, isLoading: loadingOwnedNfts } = useNFTs(nftCollection);
-
-  const { data: directListings, isLoading: loadingDirects } =
-    useValidDirectListings(marketplace, {
-      seller: router.query.address as string,
-    });
-
-  const { data: auctionListings, isLoading: loadingAuctions } =
-    useValidEnglishAuctions(marketplace, {
-      seller: router.query.address as string,
-    });
+  useEffect(() => {
+    router.push('/buy');
+  }, []);
 
   return (
-    <Container maxWidth="lg">
-      <div className={styles.tabs}>
-        <h3
-          className={`${styles.tab} ${
-            tab === "listings" ? styles.activeTab : ""
-          }`}
-          onClick={() => setTab("listings")}
-        >
-          Listed Xailiens 
-        </h3>
-        <h3
-          className={`${styles.tab} ${
-            tab === "auctions" ? styles.activeTab : ""
-          }`}
-          onClick={() => setTab("auctions")}
-        >
-          Current Auctions
-        </h3>
-      </div>
-      <div
-        className={`${tab === "listings" ? styles.activeTabContent : styles.tabContent}`}
-      >
-        {loadingDirects ? (
-          <p>Loading...</p>
-        ) : directListings && directListings.length === 0 ? (
-          <p>Nothing for sale yet! Head to the sell tab to list an NFT.</p>
-        ) : (
-          directListings?.map((listing) => (
-            <ListingWrapper listing={listing} key={listing.id} />
-          ))
-        )}
-      </div>
-      <div
-        className={`${tab === "auctions" ? styles.activeTabContent : styles.tabContent}`}
-      >
-        {loadingAuctions ? (
-          <p>Loading...</p>
-        ) : auctionListings && auctionListings.length === 0 ? (
-          <p>No Xailien available.</p>
-        ) : (
-          auctionListings?.map((listing) => (
-            <ListingWrapper listing={listing} key={listing.id} />
-          ))
-        )}
-      </div>
-    </Container>
+    <div>
+      <p>Redirecting to the buy page...</p>
+    </div>
   );
-}
+};
+
+export default IndexPage;
